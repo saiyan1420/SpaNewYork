96d1d105ddcb2eb9adbaa2de9c99eccd30de2d59
khoa le <ngocrongbc@gmail.com>
2025-03-09 22:07:10 +0700

    Update KhachHang Field

--- SpaNewYork/Areas/Admin/Controllers/DanhMucKhachHangController.cs
+++ SpaNewYork/Areas/Admin/Controllers/DanhMucKhachHangController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SpaNewYork.Models;
 
@@ -30,17 +29,10 @@ namespace SpaNewYork.Areas.Admin.Controllers
         // GET: DanhMucKhachHang/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var danhMucKhachHang = await _context.DanhMucKhachHang
-                .FirstOrDefaultAsync(m => m.STT == id);
-            if (danhMucKhachHang == null)
-            {
-                return NotFound();
-            }
+            var danhMucKhachHang = await _context.DanhMucKhachHang.FirstOrDefaultAsync(m => m.STT == id);
+            if (danhMucKhachHang == null) return NotFound();
 
             return View(danhMucKhachHang);
         }
@@ -52,14 +44,32 @@ namespace SpaNewYork.Areas.Admin.Controllers
         }
 
         // POST: DanhMucKhachHang/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("STT,LoaiKhachHang,MaKhachHang,TenKhachHang,SoDienThoai,DiaChi,GhiChu,NgaySinh")] DanhMucKhachHang danhMucKhachHang)
+        public async Task<IActionResult> Create([Bind("LoaiKhachHang,TenKhachHang,SoDienThoai,DiaChi,GhiChu,NgaySinh")] DanhMucKhachHang danhMucKhachHang)
         {
             if (ModelState.IsValid)
             {
+                // Tạo mã khách hàng tự động
+                var lastCustomer = _context.DanhMucKhachHang
+                    .Where(kh => kh.LoaiKhachHang == danhMucKhachHang.LoaiKhachHang)
+                    .OrderByDescending(kh => kh.MaKhachHang)
+                    .FirstOrDefault();
+
+                string prefix = danhMucKhachHang.LoaiKhachHang;
+                int nextNumber = 1;
+
+                if (lastCustomer != null)
+                {
+                    string lastCode = lastCustomer.MaKhachHang.Substring(2);
+                    if (int.TryParse(lastCode, out int lastNumber))
+                    {
+                        nextNumber = lastNumber + 1;
+                    }
+                }
+
+                danhMucKhachHang.MaKhachHang = $"{prefix}{nextNumber:D4}";
+
                 _context.Add(danhMucKhachHang);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -70,30 +80,20 @@ namespace SpaNewYork.Areas.Admin.Controllers
         // GET: DanhMucKhachHang/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var danhMucKhachHang = await _context.DanhMucKhachHang.FindAsync(id);
-            if (danhMucKhachHang == null)
-            {
-                return NotFound();
-            }
+            if (danhMucKhachHang == null) return NotFound();
+
             return View(danhMucKhachHang);
         }
 
         // POST: DanhMucKhachHang/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("STT,LoaiKhachHang,MaKhachHang,TenKhachHang,SoDienThoai,DiaChi,GhiChu,NgaySinh")] DanhMucKhachHang danhMucKhachHang)
         {
-            if (id != danhMucKhachHang.STT)
-            {
-                return NotFound();
-            }
+            if (id != danhMucKhachHang.STT) return NotFound();
 
             if (ModelState.IsValid)
             {
@@ -121,17 +121,11 @@ namespace SpaNewYork.Areas.Admin.Controllers
         // GET: DanhMucKhachHang/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var danhMucKhachHang = await _context.DanhMucKhachHang
                 .FirstOrDefaultAsync(m => m.STT == id);
-            if (danhMucKhachHang == null)
-            {
-                return NotFound();
-            }
+            if (danhMucKhachHang == null) return NotFound();
 
             return View(danhMucKhachHang);
         }
